fb0e7a914c975af2683b9b6e6619839f337bb540
= <==>
2020-07-21 16:09:46 +0300

    ilk dosya gönderim denemesi

---
+++ main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  System.out.println("beta proje için ilk dosya push denmesidir");
+
+
+
+
+
+  }
+}
